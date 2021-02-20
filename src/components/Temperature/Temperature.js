f13a42afe7eae4af04b754a0d20929cacbb19af8
Pegoraro, Luisa <luisapegoraro@quickenloans.com>
2021-02-19 19:45:14 -0500

    Added functionality for user to input desired city

--- src/components/Temperature/Temperature.js
+++ src/components/Temperature/Temperature.js
@@ -1,57 +1,60 @@
 import React, {Component} from 'react';
+import './Temperature.css';
 
 class Temperature extends Component{
     constructor(props){
         super(props)
         this.state = {
             temperature: 0, 
-            userInputCity: ""
+            city: "",
+            urlBase: "https://api.openweathermap.org/data/2.5/weather?q=",
+            urlAppend: "&units=imperial&appid=d7a12a132bf363c1c0ae7b8df6f2d42c",
+            isValidCity: true,
+            temperatureScale: "F"
         }
         this.selectCity = this.selectCity.bind(this);
-    }
-
-    componentDidMount(){
-        // fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=imperial&appid=d7a12a132bf363c1c0ae7b8df6f2d42c")
-        // .then(res => res.json())
-        // .then(res => {
-        //     let temp = res["main"]["temp"];
-        //     this.setState({
-        //         temperature: temp
-        //     })
-        // })
+        this.submitCity = this.submitCity.bind(this);
     }
 
     selectCity(e){
-        let city = e.target.value;
-        let urlBase = "https://api.openweathermap.org/data/2.5/weather?q=";
-        let urlAppend = "&units=imperial&appid=d7a12a132bf363c1c0ae7b8df6f2d42c";
-        let url = urlBase + city + urlAppend;
+        this.setState({
+            city: e.target.value
+        })
+    }
+
+    submitCity(e){
+        e.preventDefault();
+        let url = this.state.urlBase + this.state.city + this.state.urlAppend;
         fetch(url)
         .then(res => res.json())
         .then(res => {
-            console.log(res);
-            let temp = res["main"]["temp"];
             this.setState({
-                temperature: temp
+                temperature: res["main"]["temp"]
+            })
+        })
+        .catch(err => {
+            this.setState({
+                isValidCity: false
             })
         })
     }
 
     render(){
         return (
-        <div>
-            The temperature is: {this.state.temperature}.
-            <div>
-                <form>
+            <div className="temperatureWrapper">
+                <form onSubmit={this.submitCity}>
                     <label>
-                        Search for city:
-                        <input type="text" value={this.state.userInputCity} onChange={this.selectCity}/> 
-    
+                        Search for a city to see current temperature: &nbsp;
+                        <input type="text" value={this.state.city} onChange={this.selectCity}/> 
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
+                {  
+                    !this.state.isValidCity ? 
+                    <div> {this.state.city} is not a valid city</div> :  
+                    <p> The temperature in {this.state.city} is: {this.state.temperature}{this.state.temperatureScale} </p> 
+                }
             </div>
-        </div>
         )
     }
 }
